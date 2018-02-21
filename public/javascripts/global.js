9be091aa0ea93473c51337aa356bdc4dc917751d
yaburi <nathan.au@hotmail.com>
2018-02-21 18:52:30 +1100

    delete feature added. will create an update user feature and fix styling.

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
     // Username link click
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
 
+    // Add User button click
+    $('#btnAddUser').on('click', addUser);
+
+    // Delete User link click
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
+
     // Populate the user table on initial page load
     populateTable();
 });
@@ -58,4 +64,90 @@ function showUserInfo(event) {
     $('#userInfoAge').text(thisUserObject.age);
     $('#userInfoGender').text(thisUserObject.gender);
     $('#userInfoLocation').text(thisUserObject.location);
+}
+
+// Add User
+function addUser(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#addUser input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var newUser = {
+            'username': $('#addUser fieldset input#inputUserName').val(),
+            'email': $('#addUser fieldset input#inputUserEmail').val(),
+            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+            'age': $('#addUser fieldset input#inputUserAge').val(),
+            'location': $('#addUser fieldset input#inputUserLocation').val(),
+            'gender': $('#addUser fieldset input#inputUserGender').val()
+        }
+
+        // Use AJAX to post the object to our addUser service (in users.js)
+        $.ajax({
+            type: 'POST',
+            data: newUser,
+            url: '/users/adduser',
+            dataType: 'JSON'
+        }).done(function(response) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Clear the form inputs
+                $('#addUser fieldset input').val('');
+
+                // Update the table
+                populateTable();
+            } else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    } else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+}
+
+// Delete User
+function deleteUser(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user is confirmed
+    if (confirmation === true) {
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function(response) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+
+            } else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+        });
+    } else {
+
+        // No to confirmation
+        return false;
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -8,12 +8,35 @@ router.get('/', function(req, res, next) {
 });
 */
 
-// Get UserList
+// GET UserList
 router.get('/userlist', function(req, res) {
     var db = req.db;
     var collection = db.get('userlist');
     collection.find({},{}, function(e, docs) {
         res.json(docs);
+    });
+});
+
+// POST to add user
+router.post('/adduser', function(req,res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result) {
+        res.send(
+            // return '' if good, err if bad
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+// DELETE to delete user
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+
+    collection.remove({ '_id': userToDelete }, function(err) {
+        res.send((err === null) ? { msg: ''} : { msg: 'Error: ' + err });
     });
 });
 
